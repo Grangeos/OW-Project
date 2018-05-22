37835b762d30ab06293e6a5b13bf19d21fb3d42c
Grangeos <grangeraphael74@gmail.com>
2018-05-22 16:10:32 +0200

    edit Bootstrap v2

--- src/js/Sombra.js
+++ src/js/Sombra.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
 
 class Sombra extends Component {
   render() {
     return (
         <section>
-            coucou
+            <Alert bsStyle="warning">
+                <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+                good.
+            </Alert>
         </section>
     );
   }
