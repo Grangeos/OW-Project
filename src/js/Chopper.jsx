317c346b6e3278b1fa974041c4a3cd96b75935f1
Tibomat <thibault.noel38@gmail.com>
2018-05-23 17:46:57 +0200

    chopper

--- src/js/Chopper.jsx
+++ src/js/Chopper.jsx
@@ -1,30 +1,97 @@
 import React, {Component} from 'react';
-import {ToggleButtonGroup, ToggleButton} from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import {Button, Collapse, Image, Well, Row, Col, Grid} from 'react-bootstrap';
+
+import Arme from '../scss/picture/Armechopper.png';
+import Healchopper from '../scss/picture/Healchopper.png';
+import Grapchopper from '../scss/picture/Grapchopper.png';
+import Ultchopper from '../scss/picture/Ultchopper.png';
 
 class Chopper extends React.Component {
 	constructor(props, context) {
 		super(props, context);
 
-		this.handleChange = this.handleChange.bind(this);
-
 		this.state = {
-			value: []
+			activeTab: 0
 		};
 	}
 
-	handleChange(e) {
-		this.setState({value: e});
+	setActiveTab(index) {
+		if (this.state.activeTab === index) {
+			this.setState({ activeTab: -1 });
+		} else {
+			this.setState({
+				activeTab: index
+			});
+		}
 	}
 
 	render() {
+		const { activeTab } = this.state;
+
 		return (
-			<section className="container">
-				<ToggleButtonGroup type="checkbox" value={this.state.value} onChange={this.handleChange} justified="justified">
-					<ToggleButton value={1}>DÉFERRAILLEUR</ToggleButton>
-					<ToggleButton value={2}>INHALATEUR</ToggleButton>
-					<ToggleButton value={3}>TRAQUELARD</ToggleButton>
-					<ToggleButton value={4}>ÉQUARISSEUR</ToggleButton>
-				</ToggleButtonGroup>
+
+			<section className="container chopper">
+			<Row>
+				<Collapse in={activeTab === 0}>
+					<div>
+						<Well>
+							<p>DÉFERRAILLEUR</p>
+							<p>Le déferrailleur de Chopper lance du shrapnel à courte portée mais sur une large zone. Sinon il peut lancer une boule de shrapnels qui détone plus loin, éparpillant les fragments métalliques depuis le point d’impact.</p>
+						</Well>
+					</div>
+				</Collapse>
+				<Collapse in={activeTab === 1}>
+					<div>
+						<Well>
+							<p>INHALATEUR</p>
+							<p>Chopper récupère une partie de ses points de vie sur une courte période.</p>
+						</Well>
+					</div>
+				</Collapse>
+				<Collapse in={activeTab === 2}>
+					<div>
+						<Well>
+							<p>TRAQUELARD</p>
+							<p>Chopper lance sa chaîne vers une cible ; s’il l’attrape, il l’attire à lui, à courte portée.</p>
+						</Well>
+					</div>
+				</Collapse>
+				<Collapse in={activeTab === 3}>
+					<div>
+						<Well>
+							<p>ÉQUARISSEUR</p>
+							<p>Après avoir ajouté un chargeur supplémentaire au-dessus de son déferrailleur, Chopper le bourre de munitions. Pendant une courte période, il mitraille une large zone devant lui avec des shrapnels qui repoussent les ennemis.</p>
+						</Well>
+					</div>
+				</Collapse>
+			</Row>
+			<Row>
+				<Col xs={2}>
+					<Button block onClick={() => this.setActiveTab(0)}>
+						<Image src={Arme} alt="Arme" responsive="responsive"/>
+					</Button>
+				</Col>
+
+				<Col xs={2}>
+					<Button block onClick={() => this.setActiveTab(1)}>
+						<Image src={Healchopper} alt="Healchopper" responsive="responsive"/>
+					</Button>
+				</Col>
+
+				<Col xs={2}>
+					<Button block onClick={() => this.setActiveTab(2)}>
+						<Image src={Grapchopper} alt="Grapchopper" responsive="responsive"/>
+					</Button>
+				</Col>
+
+				<Col xs={2}>
+					<Button block onClick={() => this.setActiveTab(3)}>
+						<Image src={Ultchopper} alt="Ultchopper" responsive="responsive"/>
+					</Button>
+				</Col>
+
+			</Row>
 			</section>
 		);
 	}
