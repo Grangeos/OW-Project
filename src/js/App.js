d9d396f0b10dc86cf94c220f9031f5175d0f7a79
Grangeos <grangeraphael74@gmail.com>
2018-05-23 11:33:46 +0200

    fix bug

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import Header from "./Header.jsx";
+import Sombra from './Sombra';
 import SombraCarousel from './SombraCarousel'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = () => (
+    <SombraCarousel />
+)
 
 class App extends Component {
   render() {
@@ -10,7 +15,8 @@ class App extends Component {
             <Fragment>
                 <Header />
                 <Switch>
-                    <Route path="/" exact component={SombraCarousel} />
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/sombra" exact component={Sombra} />
                 </Switch>
             </Fragment>
         </Router>
