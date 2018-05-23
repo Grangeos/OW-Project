e9b816e283426b200b7b98aab428a9eaa0b3806b
Tibomat <thibault.noel38@gmail.com>
2018-05-23 10:33:28 +0200

    chopper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/main.css';
+import './scss/main.min.css';
 import App from '../src/js/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/js/App.js
+++ src/js/App.js
@@ -1,20 +1,18 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import Header from "./Header";
-import SombraCarousel from './SombraCarousel.js'
-
+import SombraCarousel from './SombraCarousel'
+import Chopper from './Chopper'
 
 class App extends Component {
-  render() {
-    return (
-      <Fragment>
-      <Header />
-      <SombraCarousel />
-
-
-      </Fragment>
-
-    );
-  }
+	render() {
+		return (
+			<Fragment>
+				<Header/>
+				<SombraCarousel/>
+				<Chopper/>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
