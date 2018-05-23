fae630c215a8d082028a396f450a65a09e90c678
Grangeos <37339325+Grangeos@users.noreply.github.com>
2018-05-23 11:40:10 +0200

    Raph sombra page (#10)
    * TA GUEUL ET ACCEPTE
    
    * fix bug

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import Header from "./Header.jsx";
+import Sombra from './Sombra';
+import Chopper from './Chopper';
 import SombraCarousel from './SombraCarousel'
-import Chopper from './Chopper'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = () => (
+    <SombraCarousel />
+)
 
 class App extends Component {
   render() {
@@ -11,8 +16,9 @@ class App extends Component {
             <Fragment>
                 <Header />
                 <Switch>
-                    <Route path="/sombra" exact component={SombraCarousel} />
-					<Route path="/chopper" exact component={Chopper} />
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/sombra" exact component={Sombra} />
+                    <Route path="/chopper" exact component={Chopper} />
                 </Switch>
             </Fragment>
         </Router>
---
+++ src/js/Sombra.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import SombraCarousel from './SombraCarousel';
+
+class Sombra extends Component {
+  render() {
+    return (
+        <div>
+            
+        </div>
+
+    );
+  }
+}
+
+export default Sombra;
