6ba2541a9513d8616255f41a0d8678feb25f634e
Tibomat <thibault.noel38@gmail.com>
2018-05-24 11:55:41 +0200

    chopper

--- src/js/Chopper.jsx
+++ src/js/Chopper.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Button, Collapse, Image, Well, Row, Col, Grid} from 'react-bootstrap';
+import cx from 'classnames'
+import {Button, Collapse, Image, Well, Row, Col, Grid, Jumbotron} from 'react-bootstrap';
 
 import Arme from '../scss/picture/Armechopper.png';
 import Healchopper from '../scss/picture/Healchopper.png';
 import Grapchopper from '../scss/picture/Grapchopper.png';
 import Ultchopper from '../scss/picture/Ultchopper.png';
+import ChopperArme from '../scss/picture/ChopperArme.png';
+import TakeBreather from '../scss/picture/TakeBreather.png';
+import ChopperGrap from '../scss/picture/ChopperGrap.png';
+import ChopperUlt from '../scss/picture/ChopperUlt.png';
+import ChopperFiche from '../scss/picture/ChopperFiche.png';
 
 class Chopper extends React.Component {
 	constructor(props, context) {
@@ -29,69 +35,65 @@ class Chopper extends React.Component {
 	render() {
 		const { activeTab } = this.state;
 
+		const data = [
+			{
+				img: Arme,
+				titre: "DÉFERRAILLEUR",
+				desc: "Le déferrailleur de Chopper lance du shrapnel à courte portée mais sur une large zone. Sinon il peut lancer une boule de shrapnels qui détone plus loin, éparpillant les fragments métalliques depuis le point d’impact.",
+				imgdesc: ChopperArme
+			},
+			{
+				img: Healchopper,
+				titre: "INHALATEUR",
+				desc: "Chopper récupère une partie de ses points de vie sur une courte période.",
+				imgdesc: TakeBreather
+			},
+			{
+				img: Grapchopper,
+				titre: "TRAQUELARD",
+				desc: "Chopper lance sa chaîne vers une cible ; s’il l’attrape, il l’attire à lui, à courte portée.",
+				imgdesc: ChopperGrap
+			},
+			{
+				img: Ultchopper,
+				titre: "ÉQUARISSEUR",
+				desc: "Après avoir ajouté un chargeur supplémentaire au-dessus de son déferrailleur, Chopper le bourre de munitions. Pendant une courte période, il mitraille une large zone devant lui avec des shrapnels qui repoussent les ennemis.",
+				imgdesc: ChopperUlt
+			}
+		]
 		return (
 
 			<section className="container chopper">
-			<Row>
-				<Collapse in={activeTab === 0}>
-					<div>
-						<Well>
-							<p>DÉFERRAILLEUR</p>
-							<p>Le déferrailleur de Chopper lance du shrapnel à courte portée mais sur une large zone. Sinon il peut lancer une boule de shrapnels qui détone plus loin, éparpillant les fragments métalliques depuis le point d’impact.</p>
-						</Well>
-					</div>
-				</Collapse>
-				<Collapse in={activeTab === 1}>
-					<div>
-						<Well>
-							<p>INHALATEUR</p>
-							<p>Chopper récupère une partie de ses points de vie sur une courte période.</p>
-						</Well>
-					</div>
-				</Collapse>
-				<Collapse in={activeTab === 2}>
-					<div>
-						<Well>
-							<p>TRAQUELARD</p>
-							<p>Chopper lance sa chaîne vers une cible ; s’il l’attrape, il l’attire à lui, à courte portée.</p>
-						</Well>
-					</div>
-				</Collapse>
-				<Collapse in={activeTab === 3}>
-					<div>
-						<Well>
-							<p>ÉQUARISSEUR</p>
-							<p>Après avoir ajouté un chargeur supplémentaire au-dessus de son déferrailleur, Chopper le bourre de munitions. Pendant une courte période, il mitraille une large zone devant lui avec des shrapnels qui repoussent les ennemis.</p>
-						</Well>
-					</div>
-				</Collapse>
-			</Row>
-			<Row>
-				<Col xs={2}>
-					<Button block onClick={() => this.setActiveTab(0)}>
-						<Image src={Arme} alt="Arme" responsive="responsive"/>
-					</Button>
-				</Col>
-
-				<Col xs={2}>
-					<Button block onClick={() => this.setActiveTab(1)}>
-						<Image src={Healchopper} alt="Healchopper" responsive="responsive"/>
-					</Button>
-				</Col>
-
-				<Col xs={2}>
-					<Button block onClick={() => this.setActiveTab(2)}>
-						<Image src={Grapchopper} alt="Grapchopper" responsive="responsive"/>
-					</Button>
-				</Col>
-
-				<Col xs={2}>
-					<Button block onClick={() => this.setActiveTab(3)}>
-						<Image src={Ultchopper} alt="Ultchopper" responsive="responsive"/>
-					</Button>
-				</Col>
-
-			</Row>
+				<Row className="desc">
+					{
+						data.map(({ titre, desc, imgdesc }, i) => (
+							<Collapse in={activeTab === i}>
+								<div>
+									<Well>
+										<h3>{titre}</h3>
+										<p>{desc}</p>
+										<Image src={imgdesc} alt={titre} responsive="responsive"/>
+									</Well>
+								</div>
+							</Collapse>
+						))
+					}
+				</Row>
+				<Row className="button">
+					{
+						data.map(({ img, titre }, i) => (
+							<Col className={ cx({ active: activeTab === i})}>
+								<Button block onClick={() => this.setActiveTab(i)}>
+									<Image src={img} alt={titre} responsive="responsive"/>
+								</Button>
+							</Col>
+						))
+					}
+				</Row>
+				<Jumbotron>
+					<h2>Chopper</h2>
+					<p>Avec son emblématique traquelard, Chopper attrape et attire ses ennemis avant de les réduire en miettes d’un coup de déferrailleur. Il est assez robuste pour survivre à de lourds dégâts, et peut récupérer ses points de vie grâce à son inhalateur.</p>
+				</Jumbotron>
 			</section>
 		);
 	}
